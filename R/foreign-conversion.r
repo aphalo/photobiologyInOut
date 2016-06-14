f0223ec2fb78a893d97b977b631b95208748f11d
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2016-06-15 02:31:59 +0300

    Add support for reading Ocean Optics data saved by server in a Raspberry Pi board.

--- R/foreign-conversion.r
+++ R/foreign-conversion.r
@@ -1,16 +1,45 @@
+# The "export" functions are not defined as S3 methods named as.hyperSpec and
+# as.colorSpec as would have been natural, because these generics are not
+# defined in the respective packages. Defining them as S3 generics in this
+# independently developed package would be looking for future trouble. The
+# "import" functions could have been defined as "as...." S3 methods, but
+# naming symmetry between import and export functions seemed more natural.
+# Another reason is that a single class "colorSpec" corresponds to several
+# different classes from package "photobiology" and consequently the class
+# the object is to be converted to depends on the information stored as
+# attributes in a "colorSpec" object rather than on the class it belongs to. 
 
 # hyperSpec ---------------------------------------------------------------
 
 #' Convert 'hyperSpec::hyperSpec' objects
 #' 
-#' Convert between 'hyperSpec::hyperSpec' objects and collection of spectra
-#' objects (xxxx_mspct) as defined in package 'photobiology' preserving all
-#' information. Collection of spectra objects can be easily converted into
-#' long spectral objects or into tidy data frames.
+#' Convert 'hyperSpec::hyperSpec' objects into spectral objects (xxxx_spct,
+#' xxxx_mspct) as defined in package 'photobiology' and vice veersa preserving
+#' as much information as possible.
 #' 
-#' @note hyperSpec objects use memory more efficiently than spectral objects
-#' of the classes defined in package 'photobiology' while these are more
-#' flexible as they are derived from 'tbl_df' and in turn from 'data.frame'.
+#' @note Objects of class \code{hyperSpec::hyperSpec} contain metadata or class 
+#'   data from which the quantity measured and the units of expression can be 
+#'   obtained. However, units as included in the objects are not well 
+#'   documented making automatic conversion difficult. When using this function
+#'   the user may need to use parameter \code{multiplier} to scale the data to
+#'   what is expected by the object constructors defined in package
+#'   'photobiology' and use parameter \code{spct.data.var} to select the
+#'   quantity.
+#'   
+#'   \code{hyperSpec::hyperSpec} objects may use memory more efficiently
+#'   than spectral objects of the classes for collections of spectra defined in
+#'   package 'photobiology' as wavelengths are assumed to be the same for all
+#'   member spectra, and stored only once while this assumption is not made for
+#'   collections of spectra, allowing different wavelengths and lengths for the
+#'   component spectra. Wavelengths are stored for each spectrum, but as
+#'   spectral classes are derived from 'tbl_df' in many cases no redundant
+#'   copies of wavelength data will be made in memory in spite of the more
+#'   flexible semantics of the objects.
+#'   
+#' @section Warning!: Always check the sanity of the imported or exported data
+#'   values, as guessing is needed when matching the different classes, and the
+#'   functions defined here are NOT guaranteed to return valid data wihtout help
+#'   from the user through optional function arguments.
 #' 
 #' @param x hyperSpec object
 #' @param member.class character One of the spectrum classes defined in package
@@ -50,7 +79,7 @@ hyperSpec2mspct <- function(x,
                    member.class = member.class, 
                    spct.data.var = spct.data.var)
   other.vars <- setdiff(colnames(x), "spc")
-  for (r in 1:length(z)) { # nrow(x) is same as length(z)
+  for (r in seq_along(z)) { # nrow(x) is same as length(z)
     for (var in other.vars) {
       z[[r]][[var]] <- rep(x@data[[var]][r], hyperSpec::nwl(x))
     }
@@ -60,6 +89,17 @@ hyperSpec2mspct <- function(x,
                       paste(names(dim(x)), dim(x), collapse = " "),
                       '\ncolnames: ', paste(colnames(x), collapse = ", "),
                       sep = "")
+  z
+}
+
+#' @rdname hyperSpec2mspct
+#'
+#' @export
+#'
+hyperSpec2spct <- function(x, multiplier = 1, ...) {
+  y <- hyperSpec2mspct(x, multiplier = multiplier, ...)
+  z <- rbindspct(y)
+  comment(z) <- comment(x[[1]])
   z
 }
 
@@ -74,7 +114,7 @@ mspct2hyperSpec <- function(x,
   stopifnot(is.any_mspct(x))
   spct.names <- names(x)
   spct.selector <- rep(TRUE, length(x))
-  for (i in 1:length(x)) {
+  for (i in seq_along(x)) {
     temp <- x[[i]]
     s.column <- temp[[spct.data.var]] * multiplier
     wl.current <- temp[["w.length"]]
@@ -98,21 +138,50 @@ mspct2hyperSpec <- function(x,
                     .wavelength = expression("lambda/nm")))
 }
 
+#' @rdname hyperSpec2mspct
+#' 
+#' @export
+#' 
+spct2hyperSpec <- function(x, 
+                           spct.data.var = NULL,
+                           multiplier = 1,
+                           ...) {
+  y <- generic_mspct(list(x), class = class(x)[1])
+  mspct2hyperSpec(y,
+                  spct.data.var,
+                  multiplier)
+}
 
 # pavo --------------------------------------------------------------------
 
-#' Convert "pavo::rspec' objects
+#' Convert "pavo::rspec" objects
 #' 
-#' Convert between 'pavo::rspec' objects and collection of spectra
-#' objects (xxxx_mspct) as defined in package 'photobiology' preserving all
-#' information. Collection of spectra objects can be easily converted into
-#' long spectral objects or into tidy data frames.
+#' Convert between 'pavo::rspec' objects into spectral objects (xxxx_spct,
+#' xxxx_mspct) as defined in package 'photobiology'.
 #' 
-#' @note hyperSpec objects use memory more efficiently than spectral objects
-#' of the classes defined in package 'photobiology' while these are more
-#' flexible as they are derived from 'tbl_df' and in turn from 'data.frame'.
+#' @note Objects of class \code{pavo::rspec} do not contain metadata or class
+#'   data from which the quantity measured and the units of expression could be 
+#'   obtained. When using this function the user needs to use parameter 
+#'   \code{multiplier} to convert the data to what is expected by the object 
+#'   constructors defined in package 'photobiology' and use parameter 
+#'   \code{spct.data.var} to select the quantity.
+#'   
+#'   \code{pavo::rspec} objects may use memory more efficiently than spectral 
+#'   objects of the classes for collections of spectra defined in package 
+#'   'photobiology' as wavelengths are assumed to be the same for all member 
+#'   spectra, and stored only once while this assumption is not made for 
+#'   collections of spectra, allowing different wavelengths and lengths for the 
+#'   component spectra. Wavelengths are stored for each spectrum, but as 
+#'   spectral classes are derived from 'tbl_df' in many cases no redundant 
+#'   copies of wavelength data will be made in memory in spite of the more 
+#'   flexible semantics of the objects.
 #' 
-#' @param x rpec object
+#' @section Warning!: Always check the sanity of the imported or exported data
+#'   values, as guessing is needed when matching the different classes, and the
+#'   functions defined here are NOT guaranteed to return valid data wihtout help
+#'   from the user through optional function arguments.
+#' 
+#' @param x rspec object
 #' @param member.class character One of the spectrum classes defined in package
 #'   'photobiology'.
 #' @param spct.data.var character The name to be used for the 'spc' data when
@@ -120,13 +189,6 @@ mspct2hyperSpec <- function(x,
 #' @param multiplier numeric A multiplier to be applied to the 'rspc' data 
 #'   to do unit or scale conversion.
 #' @param ... currently ignored.
-#' 
-#' @note Objects of class "pavo::rspec" do not contain metadata or class data
-#'   from which the quantity measured and the units of expression could be 
-#'   obtained. When using this function the user needs to use parameter 
-#'   \code{multiplier} to convert the data to what is expected by the object
-#'   constructors defined in package 'photobiology' and use parameter
-#'   \code{spct.data.var} to select the quantity.
 #' 
 #' @export
 #' 
@@ -157,35 +219,66 @@ rspec2mspct <- function(x,
   z
 }
 
+#' @rdname rspec2mspct
+#'
+#' @export
+#'
+rspec2spct <- function(x, multiplier = 1, ...) {
+  y <- rspec2mspct(x, multiplier = multiplier, ...)
+  z <- rbindspct(y)
+  comment(z) <- comment(x[[1]])
+  z
+}
+
+
 
 # colorSpec ---------------------------------------------------------------
 
 #' Convert 'colorSpec::colorSpec' objects
 #' 
-#' Convert between 'colorSpec::colorSpec' objects and spectral
-#' objects (xxxx_spct) as defined in package 'photobiology' preserving all
-#' information.
+#' Convert 'colorSpec::colorSpec' objects into spectral objects (xxxx_spct,
+#' xxxx_mspct) as defined in package 'photobiology' and vice veersa preserving
+#' as much information as possible.
+#' 
+#' @note Objects of class \code{colorSpec::colorSpec} do not contain metadata or
+#'   class data from which the units of expression could be obtained. When using
+#'   this function the user needs to use parameter \code{multiplier} to convert 
+#'   the data to what is expected by the object constructors defined in package 
+#'   'photobiology' but should only rarely need to use parameter
+#'   \code{spct.data.var} to select the quantity.
+#'   
+#'   \code{colorSpec::colorSpec} objects may use memory more efficiently than
+#'   spectral objects of the classes for collections of spectra defined in
+#'   package 'photobiology' as wavelengths are assumed to be the same for all
+#'   member spectra, and stored only once while this assumption is not made for
+#'   collections of spectra, allowing different wavelengths and lengths for the
+#'   component spectra. Wavelengths are stored for each spectrum, but as
+#'   spectral classes are derived from 'tbl_df' in many cases no redundant
+#'   copies of wavelength data will be made in memory in spite of the more
+#'   flexible semantics of the objects.
+#' 
+#' @section Warning!: Always check the sanity of the imported or exported data
+#'   values, as guessing is needed when matching the different classes, and the
+#'   functions defined here are NOT guaranteed to return valid data wihtout help
+#'   from the user through optional function arguments.
 #' 
 #' @param x colorSpec object
-#' @param multiplier numeric A multiplier to be applied to the 'spc' data to
-#'   do unit or
-#'   scale conversion. For example "a.u." units in some examples in package
-#'   'hyperSpec' seem to have scale factors applied.
+#' @param multiplier numeric A multiplier to be applied to the 'spc' data to do
+#'   unit or scale conversion.
 #' @param ... currently ignored.
 #' 
 #' @export
 #' 
 #' @examples 
 #' 
-#' library(hyperSpec)
-#' data(laser)
-#' wl(laser) <- 
-#' list (wl = 1e7 / (1/405e-7 - wl (laser)),
-#'       label = expression (lambda / nm))
-#' laser.mspct <- hyperSpec2mspct(laser, "source_spct", "s.e.irrad")
-#' class(laser.mspct)
+#' library(colorSpec)
+#' colorSpec2mspct(Fs.5nm)
+#' colorSpec2spct(Fs.5nm)
+#' colorSpec2mspct(C.5nm)
+#' colorSpec2spct(C.5nm)
 #' 
-colorSpec2spct <- function(x, multiplier = 1, ...) {
+colorSpec2mspct <- function(x, multiplier = 1, ...) {
+  stopifnot(inherits(x, "colorSpec"))
   stopifnot(multiplier > 0)
   spct.type <- colorSpec::type(x)
   spct.quantity <- colorSpec::quantity(x)
@@ -196,46 +289,45 @@ colorSpec2spct <- function(x, multiplier = 1, ...) {
                         '"metadata path": ', spct.metadata[["path"]], "\n",
                         '"metadata header": \n', 
                         paste(spct.metadata[["header"]], collapse = "\n"), sep = "")
-  if (length(colorSpec::numSpectra(x)) > 1) {
-    warning("colorSpec object contains multiple spectra, use 'colorSpec2mspct()'")
-    return(generic_spct())
-  }
+  y <- as.data.frame(as.matrix(x) * multiplier)
+  y[["w.length"]] <- colorSpec::wavelength(x)
   if (spct.type == "light") {
     if (spct.quantity == 'power') {
-      z <- source_spct(w.length = colorSpec::wavelength(x), 
-                       s.e.irrad = as.numeric(x) * multiplier)
+      z <- photobiology::split2source_mspct(y, 
+                                            spct.data.var = "s.e.irrad")
     } else if (spct.quantity == 'photons/sec') {
-      z <- source_spct(w.length = colorSpec::wavelength(x), 
-                       s.q.irrad = as.numeric(x) * multiplier)
+      z <- photobiology::split2source_mspct(y, 
+                                            spct.data.var = "s.q.irrad")
     } else {
       stop("unkown 'quantity': ", spct.quantity)
     }
   } else if (spct.type == 'responsivity.light') {
     if (spct.quantity %in% c('power->electrical', 'power->neural', 'power->action')) {
-      z <- response_spct(w.length = colorSpec::wavelength(x), 
-                         s.e.response = as.numeric(x) * multiplier)
+      z <- photobiology::split2response_mspct(y, 
+                                              spct.data.var = "s.e.response")
     } else if (spct.quantity %in% c('photons->electrical', 'photons->neural', 'photons->action')) {
-      z <- response_spct(w.length = colorSpec::wavelength(x), 
-                         s.q.response = as.numeric(x) * multiplier)
+      z <- photobiology::split2response_mspct(y, 
+                                              spct.data.var = "s.q.response")
     } else {
       stop("unkown 'quantity': ", spct.quantity)
     }
   } else if (spct.type == 'material') {
     if (spct.quantity == 'reflectance') {
-      z <- reflector_spct(w.length = colorSpec::wavelength(x), 
-                          Rfr = as.numeric(x) * multiplier)
+      z <- photobiology::split2reflector_mspct(y, 
+                                               spct.data.var = "Rfr")
     } else if (spct.quantity == 'transmittance') {
-      z <- filter_spct(w.length = colorSpec::wavelength(x), 
-                       Tfr = as.numeric(x) * multiplier)
+      z <- photobiology::split2filter_mspct(y, 
+                                            spct.data.var = "Tfr")
     } else if (spct.quantity == 'absorbance') {
-      z <- filter_spct(w.length = colorSpec::wavelength(x), 
-                       A = as.numeric(x)  * multiplier)
+      z <- photobiology::split2filter_mspct(y, 
+                                            spct.data.var = "A")
     } else {
       stop("unkown 'quantity': ", spct.quantity)
     }
   } else {
-    z <- generic_spct(w.length = colorSpec::wavelength(x), 
-                      s.data = as.numeric(x) * multiplier)
+    return(list())
+    # z <- photobiology::split2generic_mspct(y, 
+    #                                        spct.data.var = spct.quantity)
   }
   comment(z) <- comment.spct
   scaled <- ifelse(multiplier == 1, FALSE, multiplier)
@@ -243,16 +335,145 @@ colorSpec2spct <- function(x, multiplier = 1, ...) {
   z
 }
 
-# #' @rdname colorSpec2spct
-# #' 
-# #' @export
-# #' 
-# colorSpec2mspct <- function(x, multiplier = 1, ...) {
-#   z <- list()
-#   for (name in colorSpec::specnames(x)) {
-#     z[[name]] <- colorSpec2spct(x[ , name], 
-#                                 multiplier = multiplier,
-#                                 ...)
-#   }
-#   generic_mspct(z, class = class(z[[1]]))
-# }
+#' @rdname colorSpec2mspct
+#'
+#' @export
+#'
+colorSpec2spct <- function(x, multiplier = 1, ...) {
+  y <- colorSpec2mspct(x, multiplier = multiplier, ...)
+  z <- rbindspct(y)
+  comment(z) <- comment(x[[1]])
+  z
+}
+
+#' @rdname colorSpec2mspct
+#'
+#' @export
+#'
+colorSpec2chroma_spct <- function(x, multiplier = 1, ...) {
+  spct.type <- colorSpec::type(x)
+  spct.quantity <- colorSpec::quantity(x)
+  stopifnot(spct.quantity == 'power->neural')
+  stopifnot(colorSpec::numSpectra(x) == 3)
+  stopifnot(sort(tolower(names(x))) == c("x", "y", "z"))
+  stopifnot(multiplier > 0)
+  spct.metadata <- attr(x, "metadata", exact = TRUE)
+  comment.spct <- paste('Converted from "colorSpec::colorSpec" object\n',
+                        '"type": ', spct.type, "\n",
+                        '"quantity": ', spct.quantity, "\n",
+                        '"metadata path": ', spct.metadata[["path"]], "\n",
+                        '"metadata header": \n', 
+                        paste(spct.metadata[["header"]], collapse = "\n"), sep = "")
+  y <- as.data.frame(as.matrix(x) * multiplier)
+  y[["w.length"]] <- colorSpec::wavelength(x)
+  z <- photobiology::as.chroma_spct(y)
+  comment(z) <- comment.spct
+  z
+}
+
+#' @rdname colorSpec2mspct
+#'   
+#' @param spct.data.var character The name of the variable to read spectral data
+#'   from.
+#'   
+#' @export
+#' 
+mspct2colorSpec <- function(x, 
+                            spct.data.var = NULL,
+                            multiplier = 1,
+                            ...) {
+  stopifnot(is.any_mspct(x))
+  class.mspct <- class(x)[1]
+  comment.mspct <- comment(x)
+  comment.mspct <- 
+    paste('Converted from "', class.mspct, '" object\n',
+          comment.mspct, sep = "")
+  if (class.mspct == "source_mspct") {
+    if (is.null(spct.data.var)) {
+      x <- q2e(x, action = "replace")
+      spct.data.var <- "s.e.irrad"
+    }
+    quantity <- 'power'
+  } else if (class.mspct == "response_mspct") {
+    if (is.null(spct.data.var)) {
+      x <- q2e(x, action = "replace")
+      spct.data.var <- "s.e.response"
+    }
+    quantity <- 'power->action'
+  } else if (class.mspct == "filter_mspct") {
+    if (is.null(spct.data.var)) {
+      x <- A2T(x, action = "replace")
+      spct.data.var <- "Tfr"
+    }
+    quantity <- 'transmittance'
+  } else if (class.mspct == "reflector_mspct") {
+    if (is.null(spct.data.var)) {
+      spct.data.var <- "Rfr"
+    }
+    quantity <- 'reflectance'
+  } else if (class.mspct == "object_mspct") {
+    warning("automatic conversion from 'object_spct' to 'colorSpec' not possible")
+    return(colorSpec::colorSpec(data = numeric(), 
+                                wavelength = numeric()))
+  }
+  spct.names <- names(x)
+  spct.selector <- rep(TRUE, length.out = length(x))
+  for (i in seq_along(x)) {
+    temp <- x[[i]]
+    s.column <- temp[[spct.data.var]]
+    wl.current <- temp[["w.length"]]
+    if (i == 1L) {
+      mat <- s.column
+      wl.prev <- wl.current
+    } else {
+      if (!all(wl.current == wl.prev)) {
+        spct.selector[i] <- FALSE
+        next()
+      }
+      mat <- cbind(mat, s.column) # as column!
+    }
+  }
+  if (!is.null(dim(mat))) {
+    colnames(mat) <- spct.names[spct.selector]
+    add.name <- FALSE
+  } else {
+    add.name <- TRUE
+  }
+  z <- colorSpec::colorSpec(data = mat * multiplier, 
+                            wavelength = wl.prev,
+                            quantity = quantity
+  )
+  if (add.name) {
+    colorSpec::specnames(z) <- spct.names[spct.selector]
+  }
+  attr(z, "metadata") <- list(path = "", header = comment.mspct)
+  z
+}
+
+#' @rdname colorSpec2mspct
+#' 
+#' @export
+#' 
+spct2colorSpec <- function(x, 
+                           spct.data.var = NULL,
+                           multiplier = 1,
+                           ...) {
+  y <- generic_mspct(list(x), class = class(x)[1])
+  mspct2colorSpec(y,
+                  spct.data.var,
+                  multiplier)
+}
+
+#' @rdname colorSpec2mspct
+#' 
+#' @export
+#' 
+chroma_spct2colorSpec <- function(x, 
+                                  spct.data.var = NULL,
+                                  multiplier = 1,
+                                  ...) {
+  stopifnot(is.chroma_spct(x))
+  colorSpec::colorSpec(data = as.matrix(x[ , c("x", "y", "z")]) * multiplier,
+                       wavelength = x[["w.length"]],
+                       quantity = 'power->neural')
+}
