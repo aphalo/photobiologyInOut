c54606f457773c7e09ab98d4354f337afcb2f7e5
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2025-06-28 12:16:58 +0300

    OceanView irradiance add search for lines
    Instead of fixed line numbers search for lines by field name. Add support for clipping. Add setting of how.measured attribute.

--- R/read-ooov-file.r
+++ R/read-ooov-file.r
@@ -2,7 +2,8 @@
 #' 
 #' Reads and parses the header of a processed data file as output by
 #' OceanView to extract the whole header remark field. The time field is
-#' retrieved and decoded.  OceanView is a program, that replaces SpectraSuite.
+#' retrieved and decoded. OceanView is a program from Ocean Optics that 
+#' replaces the no longer supported SpectraSuite program.
 #' 
 #' @param file character string
 #' @param date a \code{POSIXct} object to use to set the \code{"when.measured"}
@@ -18,6 +19,9 @@
 #'   \code{\link[readr]{locale}} to create your own locale that controls things
 #'   like the default time zone, encoding, decimal mark, big mark, and day/month
 #'   names.
+#' @param range a numeric vector of length two, or any other object for which 
+#'   function \code{range()} will return a range of wavelengths expressed in 
+#'   nanometres.
 #'   
 #' @return A source_spct object.
 #' @export
@@ -25,29 +29,55 @@
 #' @keywords misc
 #' 
 #' @examples
+#'
+#' # energy spectral irradiance file
 #' 
 #'  file.name <- 
 #'    system.file("extdata", "spectrum.OVIrrad", 
 #'                package = "photobiologyInOut", mustWork = TRUE)
 #'                 
-#'  ooss.spct <- 
+#'  ooov.spct <- 
 #'    read_oo_ovirrad(file = file.name,
 #'                    locale = readr::locale("en", 
 #'                                           decimal_mark = ",",
 #'                                           grouping_mark = "",
 #'                                           tz = "Europe/Paris"))
 #'  
-#'  ooss.spct
-#'  getWhenMeasured(ooss.spct)
-#'  getWhatMeasured(ooss.spct)
-#'  cat(comment(ooss.spct))
+#'  ooov.spct
+#'  getWhenMeasured(ooov.spct)
+#'  getWhatMeasured(ooov.spct)
+#'  getHowMeasured(ooov.spct)
+#'  cat(comment(ooov.spct))
+#' 
+#'  ooov_clipped.spct <- 
+#'    read_oo_ovirrad(file = file.name,
+#'                    locale = readr::locale("en", 
+#'                                           decimal_mark = ",",
+#'                                           grouping_mark = "",
+#'                                           tz = "Europe/Paris"),
+#'                    range = c(280, NA))
+#'  
+#'  ooov_clipped.spct
+#'  getWhenMeasured(ooov_clipped.spct)
+#'  getWhatMeasured(ooov_clipped.spct)
+#'  getHowMeasured(ooov_clipped.spct)
+#'  cat(comment(ooov_clipped.spct))
+#'  
+#'  # Raw intrument counts file
+#'  
+#'  file.name <- 
+#'    system.file("extdata", "spectrum.OVIrrad", 
+#'                package = "photobiologyInOut", mustWork = TRUE)
+#'  
+#'  
 #' 
 read_oo_ovirrad <- function(file,
                             date = NULL,
                             geocode = NULL,
                             label = NULL,
                             tz = NULL,
-                            locale = readr::default_locale()) {
+                            locale = readr::default_locale(),
+                            range = NULL) {
   if (is.null(tz)) {
     tz <- locale$tz
   }
@@ -66,7 +96,8 @@ read_oo_ovirrad <- function(file,
     return(photobiology::source_spct())
   }
   file_header <- scan(file = file, nlines = 16, 
-                      skip = 0, what="character", 
+                      skip = 0, what="character",
+                      blank.lines.skip = FALSE, # to get start of data 
                       sep = "\n", quiet = TRUE)
   NonASCII <- tools::showNonASCII(file_header)
   if (length(NonASCII) > 0L) {
@@ -75,12 +106,19 @@ read_oo_ovirrad <- function(file,
             "replacing with ' '.")
     file_header <- iconv(file_header, to = "ASCII", sub = " ")
   }
-  
+  ln.idx <- which(grepl("^Number of Pixels in Spectrum: ",
+                     file_header))
   npixels <- as.integer(sub("Number of Pixels in Spectrum: ", "", 
-                            file_header[12], fixed = TRUE))
+                            file_header[ln.idx], fixed = TRUE))
+  ln.idx <- which(grepl("^Spectrometer: ", file_header))
+  sr.sn <- trimws(sub("Spectrometer: ", "", file_header[ln.idx], fixed = TRUE))
+  ln.idx <- which(grepl("^User: ", file_header))
+  sr.user <- trimws(sub("User: ", "",  file_header[ln.idx], fixed = TRUE))
   
   if (is.null(date)) {
-    line02 <- sub("Date: [[:alpha:]]{3} ", "", file_header[2])
+    ln.idx <- which(grepl("^Date: ",
+                          file_header))
+    line02 <- sub("Date: [[:alpha:]]{3} ", "", file_header[ln.idx])
     if (is.null(tz)) {
       tz <- sub("^(.{16})([[:upper:]]{3,4})(.{5})$", "\\2", line02)
       if (nchar(tz) == 4) {
@@ -90,11 +128,14 @@ read_oo_ovirrad <- function(file,
     date <- lubridate::parse_date_time(line02, "mdHMSy", tz = tz, locale = "C")
   }
   
+  to.skip <- which(grepl("^>>>>>Begin", file_header))
+  
   z <- readr::read_table(
     file = file,
     col_names = c("w.length", "s.e.irrad"),
-    skip = 14,
+    skip = to.skip,
     n_max = npixels,
+    guess_max = npixels,
     col_types = readr::cols(),
     progress = FALSE,
     locale = locale
@@ -107,14 +148,23 @@ read_oo_ovirrad <- function(file,
 
     old.opts <- options("photobiology.strict.range" = NA_integer_)
   z <- photobiology::as.source_spct(z, time.unit = "second")
+  if (!is.null(range)) {
+    z <- photobiology::clip_wl(z, range)
+  }
   options(old.opts)
 
   comment(z) <-
-    paste(paste("Ocean Optics Spectra Suite irradiance file '", file, "' imported on ", 
-                lubridate::now(tzone = "UTC"), " UTC", sep = ""),
-          paste(file_header, collapse = "\n"), 
+    paste(paste("Ocean Optics Ocean View irradiance file '", basename(file), 
+                "' imported on ", 
+                lubridate::now(tzone = "UTC"), " UTC\n\nHEADER OF IMPORTED FILE:", sep = ""),
+          paste(file_header[1:to.skip], collapse = "\n"), 
           sep = "\n")
   
+  how.measured <- paste("Measured by ", sr.user, 
+                        " with Ocean Optics spectrometer with s.n. ",
+                        sr.sn, " and OceanView software.", sep = "")
+  
+  photobiology::setHowMeasured(z, how.measured)
   photobiology::setWhenMeasured(z, date)
   photobiology::setWhereMeasured(z, geocode)
   photobiology::setWhatMeasured(z, label)
