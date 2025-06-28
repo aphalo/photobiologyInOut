415e88e09d72d7dbb23a88f12e6cd04c7fc0e5df
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2025-06-28 12:22:15 +0300

    Cleanup
    Remove raw counts import function copied from SpectraView

--- R/read-ooov-file.r
+++ R/read-ooov-file.r
@@ -62,14 +62,6 @@
 #'  getWhatMeasured(ooov_clipped.spct)
 #'  getHowMeasured(ooov_clipped.spct)
 #'  cat(comment(ooov_clipped.spct))
-#'  
-#'  # Raw intrument counts file
-#'  
-#'  file.name <- 
-#'    system.file("extdata", "spectrum.OVIrrad", 
-#'                package = "photobiologyInOut", mustWork = TRUE)
-#'  
-#'  
 #' 
 read_oo_ovirrad <- function(file,
                             date = NULL,
@@ -171,78 +163,3 @@ read_oo_ovirrad <- function(file,
   attr(z, "file.header") <- file_header
   z
 }
-
-#' @rdname read_oo_ssirrad
-#' @return A raw_spct object.
-#' @export
-#' 
-read_oo_ovdata<- function(file,
-                          date = NULL,
-                          geocode = NULL,
-                          label = NULL,
-                          tz = NULL,
-                          locale = readr::default_locale()) {
-  if (is.null(tz)) {
-    tz <- locale$tz
-  }
-  
-  label.file <- paste("File: ", basename(file), sep = "")
-  if (is.null(label)) {
-    label <- label.file
-  } else if (!is.na(label)) {
-    label <- paste(label.file, label, sep = "\n")
-  }
-  
-  line01 <- scan(file = file, nlines =  1, skip = 0, 
-                 what="character", quiet = TRUE)
-  if (line01[1] != "SpectraSuite") {
-    warning("Input file was not created by SpectrSuite as expected: skipping")
-    return(photobiology::raw_spct())
-  }
-  file_header <- scan(file = file, nlines = 16, 
-                      skip = 0, what="character", 
-                      sep = "\n", quiet = TRUE)
-  
-  npixels <- as.integer(sub("Number of Pixels in Processed Spectrum: ", "", 
-                            file_header[16], fixed = TRUE))
-  
-  if (is.null(date)) {
-    line03 <- sub("Date: [[:alpha:]]{3} ", "", file_header[3])
-    if (is.null(tz)) {
-      tz <- sub("^(.{16})([[:upper:]]{3,4})(.{5})$", "\\2", line03)
-      if (nchar(tz) == 4) {
-        tz <- sub("S", "", tz)
-      }
-    }
-    date <- lubridate::parse_date_time(line03, "mdHMSy", tz = tz)
-  }
-  
-  z <- readr::read_tsv(
-    file = file,
-    col_names = c("w.length", "counts"),
-    skip = 17,
-    n_max = npixels,
-    col_types = readr::cols(),
-    progress = FALSE,
-    locale = locale
-  )
-  
-  old.opts <- options("photobiology.strict.range" = NA_integer_)
-  z <- photobiology::as.raw_spct(z)
-  options(old.opts)
-
-  comment(z) <-
-    paste(paste("Ocean Optics Spectra Suite raw counts file '", basename(file), "' imported on ", 
-                lubridate::now(tzone = "UTC"), " UTC", sep = ""),
-          paste(file_header, collapse = "\n"), 
-          sep = "\n")
-
-  photobiology::setWhenMeasured(z, date)
-  photobiology::setWhereMeasured(z, geocode)
-  photobiology::setWhatMeasured(z, label)
-  how <- "Measured with an array spectrometer."
-  photobiology::setHowMeasured(z, how)
-  attr(z, "file.header") <- file_header
-  z
-}
-
